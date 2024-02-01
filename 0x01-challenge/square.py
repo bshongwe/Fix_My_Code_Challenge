35f8b67585412808376a3f757aaf587ca9455021
bshongwe <dru.da.beat.hustle@gmail.com>
2024-01-31 18:24:28 -0800

    Task 2: Update - pycodestyle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" Square class """
+""" Defines Square class."""
 
 
 class Square():
-    """ Square class """
+    """ Square class."""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Constructor """
-        self.width = 0
+        """ Instantiation of class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,17 +17,17 @@ class Square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """ Perimeter of the square """
+        """ Perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Print the square """
+        """ Printable representation of square object."""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = Square(width=12)
+    """ Creates Square object."""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
